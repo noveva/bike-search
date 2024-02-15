6edcbc24f1409d64bd6876e29711fcdef57dd643
noveva <nat.solovieva@gmail.com>
2024-02-15 22:42:22 +0100

    Fix unit tests

--- src/app/bikes-list/bike-card/bike-card.component.spec.ts
+++ src/app/bikes-list/bike-card/bike-card.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 import { BikeCardComponent } from './bike-card.component';
 
 describe('BikeCardComponent', () => {
@@ -8,7 +9,15 @@ describe('BikeCardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [BikeCardComponent]
+      imports: [BikeCardComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: of([{ id: 1 }])
+          }
+        }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(BikeCardComponent);
--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -8,7 +10,15 @@ describe('HeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HeaderComponent]
+      imports: [HeaderComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: of([{ id: 1 }])
+          }
+        }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(HeaderComponent);
