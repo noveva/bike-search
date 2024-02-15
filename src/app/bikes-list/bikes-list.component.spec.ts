4f054b983cc006e8e4ccec8252e84a8b2ce9acdb
noveva <nat.solovieva@gmail.com>
2024-02-15 12:10:55 +0100

    Fix broken unit tests

--- src/app/bikes-list/bikes-list.component.spec.ts
+++ src/app/bikes-list/bikes-list.component.spec.ts
@@ -1,17 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BikesListComponent } from './bikes-list.component';
+import { BikesService } from '../services/bikes/bikes.service';
+import { of } from 'rxjs';
 
 describe('BikesListComponent', () => {
   let component: BikesListComponent;
   let fixture: ComponentFixture<BikesListComponent>;
 
   beforeEach(async () => {
+    const BikeServiceStub = {
+      getBikes: () => of([])
+    };
+
     await TestBed.configureTestingModule({
-      imports: [BikesListComponent]
-    })
-    .compileComponents();
-    
+      imports: [BikesListComponent],
+      providers: [{ provide: BikesService, useValue: BikeServiceStub }]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(BikesListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- src/app/services/bikes/bikes.service.spec.ts
+++ src/app/services/bikes/bikes.service.spec.ts
@@ -1,13 +1,15 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClient } from '@angular/common/http';
 import { BikesService } from './bikes.service';
 
 describe('BikesService', () => {
   let service: BikesService;
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(BikesService);
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    service = new BikesService(httpClientSpy);
   });
 
   it('should be created', () => {
